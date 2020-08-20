86561495617d2ddbda49431fc64179d750b10c8b
sdsr1230 <42228563+sdsr1230@users.noreply.github.com>
2020-08-20 20:13:01 +0800

    Create diy-part1.sh

---
+++ diy-part1.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+mkdir package/community
+pushd package/community
+git clone --depth=1 https://github.com/fw876/helloworld
+git clone --depth=1 https://github.com/rufengsuixing/luci-app-adguardhome
+git clone --depth=1 https://github.com/lisaac/luci-app-diskman
+mkdir parted
+cp luci-app-diskman/Parted.Makefile parted/Makefile
+svn co https://github.com/project-openwrt/openwrt/trunk/package/ctcgfw/subconverter
+svn co https://github.com/project-openwrt/openwrt/trunk/package/ctcgfw/jpcre2
+svn co https://github.com/project-openwrt/openwrt/trunk/package/ctcgfw/rapidjson
+popd
